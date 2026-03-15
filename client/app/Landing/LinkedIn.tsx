d693493b3c20fa675cfd9255297cd5b7e2a903f7
Harsh <pandeygautam039@gmail.com>
2026-03-16 00:04:27 +0530

    updated UI

--- client/app/Landing/LinkedIn.tsx
+++ client/app/Landing/LinkedIn.tsx
@@ -117,7 +117,7 @@ const LandingPage = () => {
 
         <div
           ref={addToRefs}
-          className="flex items-center gap-4 mb-12 bg-white/5 border border-white/10 px-6 py-3 rounded-full backdrop-blur-sm"
+          className="flex items-center gap-4 mb-12 bg-white/5 border border-white/10 px-4 py-2 rounded-full backdrop-blur-sm"
         >
           <div className="flex -space-x-3">
             <img
@@ -143,7 +143,7 @@ const LandingPage = () => {
         </div>
 
         <div ref={addToRefs}>
-          <button className="flex items-center justify-center gap-2 bg-[#ea580c] hover:bg-[#f97316] text-white px-8 py-4 rounded-xl font-medium text-lg transition-all shadow-[0_0_20px_rgba(234,88,12,0.3)] hover:shadow-[0_0_30px_rgba(234,88,12,0.5)]">
+          <button className="cursor-pointer flex items-center justify-center gap-2 bg-[#ea580c] hover:bg-[#f97316] text-white px-12 py-4 rounded-2xl font-mono font-medium text-2xl transition-all shadow-[0_0_20px_rgba(234,88,12,0.3)] hover:shadow-[0_0_30px_rgba(234,88,12,0.5)]">
             Start Journey <ArrowRight size={20} />
           </button>
         </div>
