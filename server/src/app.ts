081bf100c3d8ecb2551705142e9dd5c0fa39bb29
Harsh <pandeygautam039@gmail.com>
2026-03-18 13:34:34 +0530

    Updated APp.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_SIDE_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_SIDE_URL || "http://localhost:3000",
     credentials: true,
   }),
 );
